8083af09a87c7178f8f8e8603c47dd9e8ad2eca2
zhuangqingbin <360650538@qq.com>
2016-04-19 22:17:38 +0800

    d

--- SPIDER.py
+++ SPIDER.py
@@ -1,23 +1,13 @@
-#ecoding:utf8
-import sys
+# -*- coding:utf-8 -*-
 import urllib2
 import urllib
 import re
-import zlib
+import lxml.html
+from lxml import etree
 
-url='https://mail.qq.com/'
-data=urllib.urlencode({'u':'360650538@qq.com','p':'zqb13645979670'})
-# req=urllib2.Request(url,data)
-req=urllib2.Request(url)
-html=urllib2.urlopen(req).read()
-print html.decode('gbk').encode('utf-8')
+reload(__import__('sys')).setdefaultencoding('utf-8')
 
-# request = urllib2.Request(url)
-# request.add_header('Accept-encoding', 'gzip')
-# opener = urllib2.build_opener()
-# response = opener.open(request)
-# html = response.read()
-# gzipped = response.headers.get('Content-Encoding')
-# if gzipped:
-#     html = zlib.decompress(html, 16+zlib.MAX_WBITS)
-# print html
+url="http://wenku.baidu.com/view/6ecf16e9998fcc22bcd10df5.html?from=search"
+html=urllib2.urlopen(url).read().decode("gbk")
+# tree=etree.HTML(html)
+# print tree.xpath("//div[@id='reader-container-2']")
---
+++ python spider.py
@@ -0,0 +1,74 @@
+# -*- coding:utf-8 -*-
+from urllib import urlretrieve
+from urllib2 import urlopen
+from bs4 import BeautifulSoup
+import re
+reload(__import__('sys')).setdefaultencoding('utf-8')
+
+# save images
+# urlretrieve('http://www.baidu.com/img/baidu_jgylogo3.gif','logo.jpg')
+
+url='http://www.pythonscraping.com/pages/warandpeace.html'
+# print urlopen(url).read()
+
+#findAll(tag,attr,recursive,text,limits,keywords)
+
+# 任务的对话时红色的，名字是绿色的
+html=urlopen(url)
+bsobj=BeautifulSoup(html,"lxml")
+namelists=bsobj.findAll("span",{"class":"green"})
+# for name in namelists:
+#     # print name
+#     print name.get_text()
+
+# dialogue=bsobj.findAll("span",{"class":"red"})
+# for each in dialogue:
+#     print each.get_text()
+
+#查找网页中内容包含 the prince 的标签数量
+# namelists=bsobj.findAll(text="the prince")
+# print len(namelists)
+
+#。findAll函数的recursive是一个布尔变量，默认为TRUE，表示在查找过程中递归多层
+
+
+##导航树
+url="http://www.pythonscraping.com/pages/page3.html"
+html=urlopen(url)
+bsobj=BeautifulSoup(html,"lxml")
+#打印giftlist表格所有产品的数据行，子标签
+# for child in bsobj.find("table",{"id":"giftList"}).children:
+#     print child
+
+#处理兄弟标签，不会获取本身，只有本身之后的数据标签，
+# for sibling in bsobj.find("table",{"id":"giftList"}).tr.next_siblings:
+#     print sibling
+
+#父标签
+# print bsobj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text()
+
+#正则表达式和Beautiful结合
+# images=bsobj.findAll("img",{"src":re.compile("\.\.\/img\/gifts/img.*\.jpg")})
+# for image in images:
+#     print image['src'] #[]运算表示从标签中取得属性的值
+
+#lambda表达式,传入参数为标签tag，然后返回值是布尔值，如此进行筛选
+# bsobj.findAll(lambda tag:len(tag.attrs)==2)
+
+
+
+
+#开始采集
+url='http://en.wikipedia.org/wiki/Kevin_Bacon'
+bsobj=BeautifulSoup(urlopen(url),"lxml")
+# for link in bsobj.findAll("a"):
+#     if 'href' in link.attrs:
+#         print link.attrs['href']
+
+#进行所需要网址的筛选,三个特点，他们都在id是bodyContent的div标签中,URL连接不包含分号，URL连接都以/wiki/开头
+links=bsobj.find("div",{"id":"bodyContent"}).findAll("a",{"href":re.compile("^(/wiki/)((?!:).)*$")})
+for link in links:
+    if 'href' in link.attrs:
+        print link.attrs['href']
+
+#异常处理
