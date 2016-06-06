ad7d218ee0bc4cea6fdc42c02ba89367a58d2948
zhuangqingbin <360650538@qq.com>
2016-06-06 15:13:56 +0800

    d

--- try.py
+++ try.py
@@ -1,42 +1,64 @@
 #coding:utf8
-html='''<!DOCTYPE html>
-<html>
-<body>
+import re
+import urllib2
+from bs4 import BeautifulSoup
+import time
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-<pre>
-Each table starts with a table tag.
-Each table row starts with a tr tag.
-Each table data starts with a td tag.
-</pre>
+headers = {
+    'User - Agent':'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36'
+}
+def creaturl(stock,page):
+    t = 'http://www.iwencai.com/search?typed=1&preParams=tid%253Dreport%2526tr%253D0%2526ft%253D1%2526st%253D0&ts=1&f=1&qs=result_rewrite&selfsectsn=&querytype=&bgid=&sdate=&edate=&searchfilter=&tid=report&w='
+    l=t+stock+'&p='+str(page)
+    return l
 
-<h4>One column:</h4>
-<table border="1">
-100 200
-</table>
 
-<h4>One row and three columns:</h4>
-<table border="1">
-<tr>
-  <td>100</td>
-  <td>200</td>
-  <td>300</td>
-</tr>
-</table>
+def get(stock):
+    list=[]
+    for i in range(1,11):
+        url=creaturl(stock,i)
+        rep=urllib2.Request(url,headers=headers)
+        html = urllib2.urlopen(rep)
+        soup = BeautifulSoup(html, "lxml")
+        li = soup.findAll("p", {"class": "s_r_attr"})
+        for each in li:
+            list.append(each.get_text()[1:11])
+    return list
 
-<h4>Two rows and three columns:</h4>
-<table border="1">
-<tr>
-  <td>100</td>
-  <td>200</td>
-  <td>300</td>
-</tr>
-<tr>
-  <td>400</td>
-  <td>500</td>
-  <td>600</td>
-</tr>
-</table>
+tests=[]
+f=open("stock.txt","r")
+for i in range(1,2141):
+    html1=f.readline()
+    html=re.sub('\n','',html1)
+    tests.append(html)
+f.close()
 
-</body>
-</html>'''
 
+
+
+
+
+
+
+time1=time.time()
+dict=dict()
+for t in tests[2000:2140]:
+    dict[t]=get(t)
+
+
+
+# dict={'a':[1,5],'b':[2,2,4,5]}
+f1=open("1900-2000.txt","a")
+for key in dict:
+    f1.write(key)
+    for l in range(len(dict[key])):
+        f1.write(",")
+        f1.write(str(dict[key][l]))
+    f1.write("\n")
+f1.close()
+
+time2=time.time()
+print (time2-time1)/60
--- 小说阅读器.py
+++ 小说阅读器.py
@@ -6,17 +6,19 @@ reload(__import__('sys')).setdefaultencoding('utf-8')
 
 #links生成一致，标题 h1，后文不完整
 
-
+#定义类
 class spider():
     def __init__(self):
         print '开始爬取......'
 
+#传入网址，返回源代码
     def getsource(self,url):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36'}
         req = urllib2.Request(url, headers=headers)
         html = urllib2.urlopen(req).read()
         return html
 
+#传入网址，返回一个存有目标网址的列表
     def creatlinks(self,source,url):
         links=[]
         parts=re.findall('<a style="" href="(.*?)">',source,re.S)
@@ -24,6 +26,7 @@ class spider():
             links.append(url+each)
         return links
 
+#传入源代码，返回正则匹配后的内容
     def extra(self,context):
         name=re.findall("<h1>(.*?)</h1>",context,re.S)
 
@@ -43,8 +46,8 @@ url='http://www.xbiquge.com/12_12804/'
 source=spider.getsource(url)
 links=spider.creatlinks(source,url)
 
-f=open('xiansong.txt','a+')
-for link in links:
+f=open('novel.txt','a+')
+for link in links[:12]: #前十章
     context=spider.getsource(link)
     contextlist=spider.extra(context)
     for ev in contextlist:
